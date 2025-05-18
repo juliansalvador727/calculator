e908ddf672df25ca4108075efd08e59fea3240e2
juliansalvador727 <juliansalvador727@gmail.com>
2025-05-18 10:42:47 -0700

    basic calculator functions

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const pi = 3.14;
+const e = 2.71;
+
+console.log(add(e, pi));
+console.log(subtract(pi, e));
+console.log(multiply(pi, e));
+console.log(divide(pi, e));
